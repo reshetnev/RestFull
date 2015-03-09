61a26f9193b67f71d23357a2f4a72e3752a24fde
reshetnev <a.v.reshetnev@gmail.com>
2015-03-09 21:58:47 +0200

    Update rest.ful

--- rest.ful/src/main/java/com/epam/reshetnev/restful/RestClient.java
+++ rest.ful/src/main/java/com/epam/reshetnev/restful/RestClient.java
@@ -9,17 +9,17 @@ import com.sun.jersey.api.client.WebResource;
 public class RestClient {
 	
     public static void main(String[] args) {
-        Client client = Client.create();
-        
-        WebResource webResource = client.resource("http://localhost:8080/rest.ful/rest/users");
-        ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-        String output = response.getEntity(String.class);
-        System.out.println(output);
-        
-        webResource = client.resource("http://localhost:8080/rest.ful/rest/users/2");
-        response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-        output = response.getEntity(String.class);
-        System.out.println(output);
+//        Client client = Client.create();
+//        
+//        WebResource webResource = client.resource("http://localhost:8080/rest.ful/rest/users");
+//        ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+//        String output = response.getEntity(String.class);
+//        System.out.println(output);
+//        
+//        webResource = client.resource("http://localhost:8080/rest.ful/rest/users/2");
+//        response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+//        output = response.getEntity(String.class);
+//        System.out.println(output);
     }
 
 }
